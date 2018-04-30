dca89c5b15834619e5057a9a796684a47d888e0e
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-04-30 22:53:40 +0000

    Add #my_count, #my_map, #my_inject to Enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -20,24 +20,55 @@ module Enumerable
   end
   
   def my_all?
-    if block_given?
-      self.my_each { |item| return false unless yield(item) }
-    else
-      self.my_each { |item| return false unless item }
-    end
+    return self.my_all? { |item| item } if !block_given?
+    self.my_each { |item| return false unless yield(item) }
     true
   end
   
   def my_any?
-    if block_given?
-      self.my_each { |item| return true if yield(item) }
-    else
-      self.my_each { |item| return true if item }
-    end
+    return self.my_any? { |item| item } if !block_given?
+    self.my_each { |item| return true if yield(item) }
     false
   end
   
   def my_none?(&block)
     !self.my_any?(&block)
   end
+  
+  def my_count(value = omitted = true)
+    if !block_given?
+      return self.my_count { true } if omitted
+      return self.my_count { |item| item == value }
+    end
+    count = 0
+    self.my_each { |item| count += 1 if yield(item) }
+    count
+  end
+  
+  def my_map(proc = omitted = true)
+    result = []
+    if !omitted
+      self.my_each { |item| result << proc.call(item) }
+    else
+      self.my_each { |item| result << yield(item) }
+    end
+    result
+  end
+  
+  def my_inject(initial_value = omitted = true)
+    if omitted
+      result = self.first
+      array = self[1..-1]
+    else
+      result = initial_value
+      array = self
+    end
+    array.my_each { |item| result = yield(result, item) }
+    result
+  end
+end
+
+
+def multiply_els(array)
+  array.my_inject { |a, b| a * b }
 end
