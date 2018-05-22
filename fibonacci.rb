21745668755ad3443d9d7f1c2d9c17104e648160
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-05-22 00:01:36 +0000

    Fix fibonacci solutions

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,10 @@
 def fibs(n)
   this_number = 1
-  last_number = 0
+  last_number = 1
   (n-2).times { this_number, last_number = this_number + last_number, this_number }
   n == 0 ? 0 : this_number
 end
 
 def fibs_rec(n)
-  n < 2 ? n : n + fibs_rec(n-1)
+  n < 2 ? n : fibs_rec(n-1) + fibs_rec(n-2)
 end
