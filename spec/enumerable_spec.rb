8b27f96aa745af2404277e63ca858b387a9fba6c
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-06-06 23:58:01 +0000

    Add more tests for Enumerable methods

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,7 +4,7 @@ describe Enumerable do
   let(:letters) { ["a", "b", "c", "d", "e"] }
   let(:albums) { { "Fakebook" => 1990, "Painful" => 1993, "Fade" => 2013 } }
 
-  describe "the my_select method" do
+  describe "#my_select" do
     
     it "processes an empty array" do
       expect([].my_select { |i| true }).to eq([])
@@ -24,6 +24,75 @@ describe Enumerable do
     
     it "works on hashes" do
       expect(albums.my_select { |k, v| v > 2000 }).to eq([["Fade", 2013]])
+    end
+  end
+  
+  describe "#my_all?" do
+    it "processes an empty array" do
+      expect([].my_all? { |i| false }).to eq(true)
+    end
+    
+    it "returns true if block is always true" do
+      expect(letters.my_all? { |i| true }).to eq(true)
+    end
+    
+    it "returns false if block is always false" do
+      expect(letters.my_all? { |i| false }).to eq(false)
+    end
+    
+    it "returns false if block is false at least once" do
+      expect(letters.my_all? { |i| i > "c" }).to eq(false)
+    end
+    
+    it "works with hashes" do
+      expect(albums.my_all? { |k, v| v > 1980 }).to eq(true)
+    end
+  end
+  
+  describe "#my_count" do
+    it "returns 0 given an empty array" do
+      expect([].my_count).to eq(0)
+    end
+    
+    it "works with populated arrays" do
+      expect(letters.my_count).to eq(5)
+    end
+    
+    it "works with hashes" do
+      expect(albums.my_count).to eq(3)
+    end
+  end
+  
+  describe "#my_map" do
+    it "returns an empty array given an empty array" do
+      expect([].my_map).to eq([])
+    end
+    
+    it "functions correctly given a static block" do
+      expect(letters.my_map { |i| true }).to eq([true] * 5)
+    end
+    
+    it "functions correctly given a dynamic block" do
+      expect(letters.my_map { |i| i.upcase }).to eq(["A", "B", "C", "D", "E"])
+    end
+    
+    it "works with hashes" do
+      expect(albums.my_map { |k, v| k.upcase })
+          .to eq(["FAKEBOOK", "PAINFUL", "FADE"])
+    end
+  end
+  
+  describe "#my_inject" do
+    it "works with empty array" do
+      expect([].my_inject(3) { |a, b| a+b }).to eq(3)
+    end
+    
+    it "works without initial value" do
+      expect([1, 2, 3].my_inject { |a, b| a+b }).to eq(6)
+    end
+    
+    it "works with populated array and initial value" do
+      expect([1, 2, 3].my_inject(4) { |a, b| a+b }).to eq(10)
     end
   end
 end
